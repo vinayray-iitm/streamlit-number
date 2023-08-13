c0fb6b1549507644c3d345d5a8bc6296cea4d05d
vinayray-iitm <142171161+vinayray-iitm@users.noreply.github.com>
2023-08-13 23:23:45 +0530

    app.py

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ import pickle
 
 st.write("""
 # Credit Card Approval Prediction App
+
+This app predicts the credit card approval probablity
+""")
+#Get Input
+
 val1 = input("Enter your value: ")
 val2 = input("Enter your value: ")
 if val1 > val2:
@@ -13,9 +18,6 @@ if val1 > val2:
 else:
     print({val2} is greater)
 
-This app predicts the credit card approval probablity
-""")
-#Get Input
 
 st.header('User Input Parameters')
 st.subheader('User Input parameters')
