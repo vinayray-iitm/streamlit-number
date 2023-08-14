ebf81c00dd405abe261bcadabab6861cca737e31
vinayray-iitm <142171161+vinayray-iitm@users.noreply.github.com>
2023-08-14 12:38:23 +0530

    app.py

--- app.py
+++ app.py
@@ -11,14 +11,18 @@ This app will compare two given  numbers
 """)
 #Get Input
 
-value1  = st.number_input("select a number", min_value = 0)
-value2  = st.number_input("select another number", min_value = 0)
-value3  = st.number_input("select one more number", min_value = 0)
-
-if value1 > value2:
-    st.write("value1 is greater")
+num1  = st.number_input("select a number", min_value = 0)
+num2  = st.number_input("select another number", min_value = 0)
+num3  = st.number_input("select one more number", min_value = 0)
+if (num1 > num2) and (num1 > num3):
+        largest_num = num1
+elif (num2 > num1) and (num2 > num3):
+        largest_num = num2
 else:
-    st.write("value2 is greater")
+        largest_num = num3
+st.header('Result')
+st.subheader("The largest of the 3 numbers is : ", largest_num)
+    
 
 st.header('User Input Parameters')
 st.subheader('User Input parameters')
