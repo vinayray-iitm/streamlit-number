56f6eb320f73c899399984fee40ca1aff65f71c6
vinayray-iitm <142171161+vinayray-iitm@users.noreply.github.com>
2023-08-15 11:44:10 +0530

    app.py

--- app.py
+++ app.py
@@ -16,14 +16,14 @@ num2  = st.number_input("select another number num2", min_value = 0)
 num3  = st.number_input("select one more number num3", min_value = 0)
 if (num1 > num2) and (num1 > num3):
         st.header('Result')
-        st.subheader("The largest of the 3 numbers is num1 ")
+        st.subheader("The largest of the 3 numbers is : ", num1)
 elif (num2 > num1) and (num2 > num3):
         
         st.header('Result')
-        st.subheader("The largest of the 3 numbers is num2 ")
+        st.subheader("The largest of the 3 numbers is :", num2)
 else:
         
         st.header('Result')
-        st.subheader("The largest of the 3 numbers is num3 ")
+        st.subheader("The largest of the 3 numbers is :" , num3)
 
     
