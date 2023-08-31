16694a37dc90b45babd4e0c29b11aa67c1b9c552
vinayray-iitm <142171161+vinayray-iitm@users.noreply.github.com>
2023-08-31 13:02:30 +0530

    app.py

--- app.py
+++ app.py
@@ -14,8 +14,9 @@ This app will compare two given  numbers
 num1  = st.number_input("select a number : num1", min_value = 0)
 num2  = st.number_input("select another number : num2", min_value = 0)
 num3  = st.number_input("select one more number :  num3", min_value = 0)
-if (num1 > num2) and (num1 > num3):
-        st.header('Result' , num1)
+if (num1 > num2):
+        if  (num1 > num3):
+           st.header('Result' , num1)
         #st.subheader("The largest of the 3 numbers is : ", num1)
 # elif (num2 > num1) and (num2 > num3):
         
